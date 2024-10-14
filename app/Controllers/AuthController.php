1ad29b379b2af5ab3d5b86a98fdb88c779c1ecc0
ArieAnggara <ariaggr2311@gmail.com>
2024-10-14 13:30:18 +0700

    setting login pengusul

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -204,49 +204,6 @@ class AuthController extends BaseController
         }
     }
 
-
-    // public function createRegister()
-    // {
-    //     $model = new PengusulModel();
-
-    //     $email = $this->request->getPost('email');
-    //     $existingUser = $model->where('email', $email)->first();
-
-    //     if ($existingUser) {
-    //         return $this->response->setJSON(['success' => false, 'errors' => 'Email sudah terdaftar']);
-    //     }
-
-    //     $file = $this->request->getFile('surat_pengantar');
-    //     $filePath = '';
-
-    //     if ($file && $file->isValid() && !$file->hasMoved()) {
-    //         if ($file->getClientMimeType() == 'application/pdf') {
-    //             $filePath = $file->store('suratpengantar', $file->getRandomName());
-    //         } else {
-    //             return $this->response->setJSON(['success' => false, 'errors' => 'Invalid file type. Only PDF files are allowed']);
-    //         }
-    //     }
-
-    //     $data = [
-    //         'jenis_instansi' => $this->request->getPost('jenis_instansi'),
-    //         'nama_instansi_pribadi' => $this->request->getPost('nama_instansi_pribadi'),
-    //         'provinsi' => $this->request->getPost('provinsi'),
-    //         'telepon' => $this->request->getPost('telepon'),
-    //         'email' => $email,
-    //         'kata_sandi' => password_hash($this->request->getPost('kata_sandi'), PASSWORD_DEFAULT),
-    //         'role_akun' => 'Pengusul',
-    //         'status_akun'  => 'Pending',
-    //         'surat_pengantar' => $filePath
-    //     ];
-
-    //     if ($model->insert($data)) {
-    //         return $this->response->setJSON(['success' => true]);
-    //     } else {
-    //         log_message('error', 'Registration failed: ' . json_encode($model->errors()));
-    //         return $this->response->setJSON(['success' => false, 'errors' => $model->errors()]);
-    //     }
-    // }
-
     public function downloadSuratPengantar($filename)
     {
         $filePath = WRITEPATH . 'uploads/suratpengantar/' . $filename;
