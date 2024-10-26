1eac8863a4dbe27c518e154159c8e4cebdd4994d
ArieAnggara <ariaggr2311@gmail.com>
2024-10-26 16:36:28 +0700

    penambahan tombol kembali untuk halaman tambah arsip penerima

--- app/Views/admin/editpamflet.php
+++ app/Views/admin/editpamflet.php
@@ -11,11 +11,11 @@
 <body class="lg:flex">
 
   <!-- Sidebar -->
-  
+
   <div class="w-64 bg-white text-white">
     <?= $this->include('template/sidebaradmin') ?>
   </div>
-  
+
 
   <div class="lg:flex-1 p-6">
     <div class="min-h-screen flex flex-col">
@@ -40,7 +40,7 @@
                 <th class="p-4 transition-colors cursor-pointer border-b border-slate-300 bg-slate-50 hover:bg-slate-100">
                   <p class="flex items-center justify-center gap-2 text-sm font-bold leading-none text-slate-800">Tanggal</p>
                 </th>
-                
+
                 <th class="p-4 transition-colors cursor-pointer border-b border-slate-300 bg-slate-50 hover:bg-slate-100">
                   <p class="flex items-center justify-center gap-2 text-sm font-bold leading-none text-slate-800">Upload Pamflet</p>
                 </th>
@@ -52,35 +52,37 @@
             <tbody>
               <!-- Example row -->
               <tr class="hover:bg-slate-50">
-                <td class="p-4 border-b border-slate-200 text-center"><p class="block text-sm text-slate-800">Pamflet Pendaftaran Kalpataru 2024</p></td>
-                <td class="p-4 border-b border-slate-200 text-center"><p class="block text-sm text-slate-800">2024-10-22</p></td>
-                
                 <td class="p-4 border-b border-slate-200 text-center">
-                <div class="flex items-center">
-                        </div>
-                        <input id="surat_pengantar" type="file" accept="application/pdf" name="surat_pengantar"
-                            class="border-2 border-gray-300 text-primary text-xs rounded-lg p-2 transition ease-in-out duration-150 focus:border-primary hover:border-primary focus:outline-none">
-                            </div>
-                            
-                        </div>
+                  <p class="block text-sm text-slate-800">Pamflet Pendaftaran Kalpataru 2024</p>
                 </td>
-                <td class="p-4 text-center border-b border-slate-200">
-                <form id="filterForm2" action="" method="post" class="flex items-center justify-center">
-                  <select name="status" id="statusSelect" class="ml-2 border-2 border-primary text-primary rounded-md shadow-sm">
-                    <option value="Terkirim">Terbit</option>
-                    <option value="Administrasi">Ditangguhkan</option>
-                  </select>
-                </form>
-              </td>
-              </tr>
-              <!-- Repeat the above block for more rows -->
-            </tbody>
-          </table>
+                <td class="p-4 border-b border-slate-200 text-center">
+                  <p class="block text-sm text-slate-800">2024-10-22</p>
+                </td>
+
+                <td class="p-4 border-b border-slate-200 text-center">
+                  <div class="flex items-center">
+                  </div>
+                  <input id="surat_pengantar" type="file" accept="application/pdf" name="surat_pengantar"
+                    class="border-2 border-gray-300 text-primary text-xs rounded-lg p-2 transition ease-in-out duration-150 focus:border-primary hover:border-primary focus:outline-none">
         </div>
 
-        
       </div>
+      </td>
+      <td class="p-4 text-center border-b border-slate-200">
+        <form id="filterForm2" action="" method="post" class="flex items-center justify-center">
+          <select name="status" id="statusSelect" class="ml-2 border-2 border-primary text-primary rounded-md shadow-sm">
+            <option value="Terkirim">Terbit</option>
+            <option value="Administrasi">Ditangguhkan</option>
+          </select>
+        </form>
+      </td>
+      </tr>
+      <!-- Repeat the above block for more rows -->
+      </tbody>
+      </table>
     </div>
+  </div>
+  </div>
   </div>
 
   <!-- Modal Pop-up -->
@@ -90,7 +92,7 @@
       <form>
         <label for="newPassword" class="block text-sm font-medium text-gray-700">Kata Sandi Baru</label>
         <input id="newPassword" type="password" class="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary" placeholder="Masukkan kata sandi baru">
-        
+
         <div class="mt-6 flex justify-end">
           <button type="button" id="cancelButton" class="bg-rejected text-white px-4 py-2 rounded-lg mr-2">Batal</button>
           <button type="submit" class="bg-primary text-white px-4 py-2 rounded-lg">Simpan</button>
