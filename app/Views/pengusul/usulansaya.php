24a400a99fa2b4dd6340cd07aeb8e79cb05a914e
aisyahwulandr <wul2003@gmail.com>
2024-11-02 23:55:10 +0700

    tooltip usulan saya ok

--- app/Views/pengusul/usulansaya.php
+++ app/Views/pengusul/usulansaya.php
@@ -183,42 +183,66 @@
                                         <td class="p-4 border-b border-slate-200 text-center">
                                             <button class="lihatButton w-20 rounded-md py-2 px-2 text-center font-semibold text-xs text-primary bg-secondary" type="button" onclick="showCatatanModal('catatanModal<?= $no; ?>')">Lihat</button>
                                         </td>
-                                        <td class="border-b border-slate-200 text-center viewCell">
-                                            <a href="./detailusulansaya/<?= $u['id_pendaftaran']; ?>">
+                                        <td class="border-b border-slate-200 text-center viewCell relative">
+                                            <a href="./detailusulansaya/<?= $u['id_pendaftaran']; ?>" class="group relative">
                                                 <!-- Ikon Lihat -->
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="size-6">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                                 </svg>
+                                                <!-- Tooltip for Lihat -->
+                                                <span class="absolute bottom-full transform -translate-x-1/2 mb-1 hidden group-hover:inline-block bg-black bg-opacity-80 text-white text-xs rounded-md px-2 py-1 whitespace-nowrap shadow-lg">
+                                                    Lihat Detail Usulan
+                                                    <span class="absolute left-1/2 transform -translate-x-1/2 top-full border-8 border-transparent border-t-black bg-opacity-80"></span>
+                                                </span>
                                             </a>
                                         </td>
-                                        <td class="border-b border-slate-200 text-center editCell">
+
+                                        <td class="border-b border-slate-200 text-center editCell relative">
                                             <?php if (in_array($u['status_pendaftaran'], ['Draft', 'Perlu Perbaikan'])): ?>
-                                                <a href="./detailusulansayaedit/<?= $u['id_pendaftaran']; ?>">
+                                                <a href="./detailusulansayaedit/<?= $u['id_pendaftaran']; ?>" class="group relative">
                                                     <!-- Ikon Edit -->
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="size-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                                     </svg>
+                                                    <!-- Tooltip for Edit -->
+                                                    <span class="absolute bottom-full transform -translate-x-1/2 mb-1 hidden group-hover:inline-block bg-black bg-opacity-80 text-white text-xs rounded-md px-2 py-1 whitespace-nowrap shadow-lg">
+                                                        Edit Detail Usulan
+                                                        <span class="absolute left-1/2 transform -translate-x-1/2 top-full border-8 border-transparent border-t-black bg-opacity-80"></span>
+                                                    </span>
                                                 </a>
                                             <?php endif; ?>
                                         </td>
-                                        <td class="border-b border-slate-200 text-center downloadCell">
+
+                                        <td class="border-b border-slate-200 text-center downloadCell relative">
                                             <?php if (!in_array($u['status_pendaftaran'], ['Draft', 'Perlu Perbaikan'])): ?>
-                                                <a href="./pdf/<?= $u['kode_registrasi']; ?>">
+                                                <a href="./pdf/<?= $u['kode_registrasi']; ?>" class="group relative">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="size-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                                                     </svg>
+                                                    <!-- Tooltip for Download -->
+                                                    <span class="absolute bottom-full transform -translate-x-1/2 mb-1 hidden group-hover:inline-block bg-black bg-opacity-80 text-white text-xs rounded-md px-2 py-1 whitespace-nowrap shadow-lg">
+                                                        Unduh Detail Usulan
+                                                        <span class="absolute left-1/2 transform -translate-x-1/2 top-full border-8 border-transparent border-t-black bg-opacity-80"></span>
+                                                    </span>
                                                 </a>
                                             <?php endif; ?>
                                         </td>
-                                        <td class="border-b border-slate-200 text-center deleteCell">
+
+                                        <td class="border-b border-slate-200 text-center deleteCell relative">
                                             <?php if ($u['status_pendaftaran'] === 'Perlu Perbaikan'): ?>
-                                                <a href="./detailusulansayadelete">
+                                                <a href="./detailusulansayadelete" class="group relative">
                                                     <!-- Ikon Hapus -->
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="size-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                     </svg>
+                                                    <!-- Tooltip for Delete -->
+                                                    <span class="absolute bottom-full transform -translate-x-1/2 mb-1 hidden group-hover:inline-block bg-black bg-opacity-80 text-white text-xs rounded-md px-2 py-1 whitespace-nowrap shadow-lg">
+                                                        Hapus Usulan
+                                                        <span class="absolute left-1/2 transform -translate-x-1/2 top-full border-8 border-transparent border-t-black bg-opacity-80"></span>
+                                                    </span>
                                                 </a>
                                             <?php endif; ?>
+                                        </td>
                                         <td class="p-4 border-b border-slate-200 text-center">
                                             <?php if ($u['status_pendaftaran'] === 'Draft' || $u['status_pendaftaran'] === 'Perlu Perbaikan'): ?>
                                                 <button class="kirimDataButton w-20 rounded-md py-2 px-2 text-center text-xs text-white transition-all shadow-md hover:shadow-lg bg-primary hover:bg-primaryhover active:shadow-none" type="button" data-id="<?= $u['id_pendaftaran'] ?>">
